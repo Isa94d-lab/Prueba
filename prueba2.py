3dd485ff08e741b5d49e459d8736a5f8d164d24d
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2024-10-04 08:21:48 -0500

    Interfaz

--- prueba2.py
+++ prueba2.py
@@ -1,4 +1,12 @@
-"ffffffffffffffffffff"
-print("HOla mundo")
-print("si")
-
+while True:
+    print ("""
+        -----------------
+        THE CHACHIPUN
+        -----------------
+    """)
+    print("BIENVENID@! Que te gustaria hacer?")
+    print("1. Ingresar nombre de usuario")
+    print("2. Jugar multijugador")
+    print("3. Jugar contra IA")
+    print("4. Jugar multijugador")
+    break 
